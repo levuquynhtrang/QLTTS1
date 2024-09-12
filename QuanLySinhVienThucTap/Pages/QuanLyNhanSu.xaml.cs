0ee35b6c07e6929dfdbeb624f477423bc7449eaa
levuquynhtrang <tranglevuquynh@gmail.com>
2024-09-12 22:44:35 +0700

    commit features

---
+++ QuanLySinhVienThucTap/Pages/QuanLyNhanSu.xaml.cs
@@ -0,0 +1,84 @@
+﻿using QuanLySinhVienThucTap.UserControllerNEU;
+using QuanLySinhVienThucTap.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuanLySinhVienThucTap.Pages
+{
+    public partial class QuanLyNhanSu : Page
+    {
+        public QuanLyNhanSu(string PhongBanUser)
+        {
+            InitializeComponent();
+            QuanLyNhanSuViewModel qlns = new QuanLyNhanSuViewModel();
+            qlns.UserDepart = PhongBanUser;
+            DataContext = qlns;
+        }
+
+        private void QuanLyNhanSu_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (DataContext is QuanLyNhanSuViewModel viewModel)
+            {
+                viewModel.OnLoaded();
+            }
+        }
+
+        private void ListBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("ok");
+        }
+
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void ListDuAnItem_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+    }
+
+    public class StatusToColorConverterOther : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string status = value as string;
+
+            if (status == "done")
+            {
+                return new SolidColorBrush(Colors.DarkGreen);
+            }
+            else if (status == "in-progress")
+            {
+                return new SolidColorBrush(Colors.DarkGoldenrod);
+            }
+            else if (status == "expired")
+            {
+                return new SolidColorBrush(Colors.DarkRed);
+            }
+
+            return new SolidColorBrush(Colors.DarkGray);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ QuanLySinhVienThucTap/TTS_Page/TTS_DanhSachKhoaHocPage/TTS_ChiTietKhoaHocPage_Inside/TTS_NoiDungVideoKhoaHoc.xaml.cs
@@ -0,0 +1,49 @@
+﻿using QuanLySinhVienThucTap.Model;
+using QuanLySinhVienThucTap.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace QuanLySinhVienThucTap.TTS_Page.TTS_DanhSachKhoaHocPage.TTS_ChiTietKhoaHocPage_Inside
+{
+    /// <summary>
+    /// Interaction logic for TTS_NoiDungVideoKhoaHoc.xaml
+    /// </summary>
+    public partial class TTS_NoiDungVideoKhoaHoc : Page
+    {
+        private string pathInDatabase;
+        public TTS_NoiDungVideoKhoaHoc(string MaKhoaDaoTao, int VideoNo)
+        {
+            InitializeComponent();
+            F2_TTS_NoiDungVideoKhoaHocViewModel viewModel = new F2_TTS_NoiDungVideoKhoaHocViewModel();
+            DataContext = viewModel;
+            viewModel.MaKhoaDaoTao = MaKhoaDaoTao;
+            pathInDatabase = DataProvider.Ins.DB.tblNoiDungDaoTaos.Where(x => x.MaKhoaDaoTao == MaKhoaDaoTao).Select(x => x.Video).SingleOrDefault();
+
+            string videoPath = $"..\\..\\VideoFileSystem\\VideoKhoaHoc\\{pathInDatabase}";
+            mediaElement.Source = new Uri(videoPath, UriKind.RelativeOrAbsolute);
+            Loaded += TTS_NoiDungVideoKhoaHoc_Loaded;
+        }
+
+        private void TTS_NoiDungVideoKhoaHoc_Loaded(object sender, RoutedEventArgs e)
+        {
+            mediaElement.Play();
+        }
+
+    }
+
+}
